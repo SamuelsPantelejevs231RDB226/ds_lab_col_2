107499857110ae6a895b337f49ee308a1def3bd8
SamuelsPantelejevs231RDB226 <144732646+SamuelsPantelejevs231RDB226@users.noreply.github.com>
2024-04-30 15:00:57 +0000

    30.04.24

--- heap.py
+++ heap.py
@@ -57,16 +57,15 @@ class MaxHeap:
 
 
 def find_kth_smallest(nums, k):
-    ###################################
-    ###                             ###
-    ###                             ###
-    ###                             ###
-    ###################################
+    max_heap = MaxHeap()
 
+    for num in nums:
+        max_heap.insert(num)
+        if len(max_heap.heap) > k:
+            max_heap.remove()
 
+    return max_heap.remove()
 
-
-# Test cases
 nums = [[3,2,1,5,6,4], [6,5,4,3,2,1], [1,2,3,4,5,6], [3,2,3,1,2,4,5,5,6]]
 ks = [2, 3, 4, 7]
 expected_outputs = [2, 3, 4, 5]
@@ -79,35 +78,3 @@ for i in range(len(nums)):
     print(f'Expected output: {expected_outputs[i]}')
     print(f'Test passed: {result == expected_outputs[i]}')
     print('---------------------------------------')
-
-
-"""
-    EXPECTED OUTPUT:
-    ----------------
-    Test case 1...
-    Input: [3, 2, 1, 5, 6, 4] with k = 2
-    Output: 2
-    Expected output: 2
-    Test passed: True
-    ---------------------------------------
-    Test case 2...
-    Input: [6, 5, 4, 3, 2, 1] with k = 3
-    Output: 3
-    Expected output: 3
-    Test passed: True
-    ---------------------------------------
-    Test case 3...
-    Input: [1, 2, 3, 4, 5, 6] with k = 4
-    Output: 4
-    Expected output: 4
-    Test passed: True
-    ---------------------------------------
-    Test case 4...
-    Input: [3, 2, 3, 1, 2, 4, 5, 5, 6] with k = 7
-    Output: 5
-    Expected output: 5
-    Test passed: True
-    ---------------------------------------
-
-"""
-
